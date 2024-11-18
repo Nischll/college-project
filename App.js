8a3d256b3260710456d76823592f99d1ea64c586
Nischal_Shrestha <nnischal9156@gmail.com>
2024-11-18 23:48:45 +0545

    login authentication done

--- App.js
+++ App.js
@@ -3,7 +3,9 @@ const oracledb = require('oracledb');
 const cors = require('cors');
 const dbConfig = require('./dbConfig'); 
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
+const JWT_SECRET = 'mySuperSecretKey@1234';
 const app = express();
 const PORT = 3000;
 
@@ -55,6 +57,56 @@ app.post('/signup', async (req, res) => {
   }
 });
 
+// Login API
+app.post('/login', async (req, res) => {
+
+  const { email, password } = req.body;
+
+  let connection;
+
+  try {
+
+    connection = await oracledb.getConnection(dbConfig);
+
+    // Query to find user by email
+    const result = await connection.execute(
+      `SELECT email, password, role FROM signup_details WHERE email = :email`,
+      { email }
+    );
+    // console.log('Database query result:', result.rows);
+
+    if (result.rows.length === 0) {
+      // User not found
+      return res.status(404).send({ error: 'User not found' });
+    }
+
+    const [dbEmail, dbPassword, role] = result.rows[0]; // Extract email and password from the query result
+    // console.log('Email:', dbEmail, 'Password:', dbPassword, 'Role:', role);
+
+    // Compare entered password with hashed password
+    const isPasswordMatch = await bcrypt.compare(password, dbPassword);
+
+    if (!isPasswordMatch) {
+      return res.status(401).send({ error: 'Invalid email or password' });
+    }
+
+    const token = jwt.sign({ email: dbEmail }, JWT_SECRET, { expiresIn: '1h' });
+
+    // Success: Send response (or generate JWT here if needed)
+    res.status(200).send({ 
+      message: 'Login successful', 
+      token,
+      user: {email: dbEmail, role}
+    });
+  } catch (err) {
+    console.error('Error during login:', err);
+    res.status(500).send({ error: 'Failed to login' });
+  } finally {
+    if (connection) {
+      await connection.close();
+    }
+  }
+});
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
